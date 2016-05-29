9b00937ac90e33f10dc87d4650f3751e706f2db6
Roman Dubrovin <rdbrvn@mail.ru>
2016-05-29 18:50:46 +0300

    Commit

--- FirstProject/src/FirstClass.java
+++ FirstProject/src/FirstClass.java
@@ -1,7 +1,5 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("GitHub!");
-        System.out.println("How is github working?!"); // what!?
     }
 }
